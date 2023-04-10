584dee96a5d978f5eb5fd377461449327cff7fb5
Nahom <zerihunnahom@gmail.com>
2023-04-10 18:49:29 +0300

    Set up react router for navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Rockets from './components/Rockets';
+import NavBar from './components/NavBar';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
-    <div className="App" />
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<MyProfile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
