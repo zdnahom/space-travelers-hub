34137507182884c032d5603d285ae0b3873e20e3
Nahom <zerihunnahom@gmail.com>
2023-04-11 13:05:03 +0300

    Dispatch getMissions action

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,17 @@
-const Missions = () => (
-  <section>
-    missions
-  </section>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/features/missions/missionsSlice';
+
+const Missions = () => {
+  const missions = useSelector((store) => store.missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, []);
+  return (
+    <section>
+      missions
+    </section>
+  );
+};
 export default Missions;
