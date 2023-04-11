8315f65cb8023c9e9ab3fb8a7b9930548ce86ca1
Nahom <zerihunnahom@gmail.com>
2023-04-11 14:01:22 +0300

    Render missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,16 +1,38 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMissions } from '../redux/features/missions/missionsSlice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMissions } from "../redux/features/missions/missionsSlice";
 
 const Missions = () => {
-  const missions = useSelector((store) => store.missions);
+  const { missions, isLoading } = useSelector((store) => store.missions);
+  console.log(missions);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getMissions());
   }, []);
   return (
     <section>
-      missions
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div>
+          <table>
+            <tr>
+              <th>Mission</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+            {missions.map((mission) => (
+              <tr>
+                <td>{mission.mission_name}</td>
+                <td>{mission.description}</td>
+                <td>Not a member</td>
+                <td> <button type="button">Join Mission</button></td>
+              </tr>
+            ))}
+            </table>
+        </div>
+      )}
     </section>
   );
 };
