0daf7419c081b96148def29a04846520374b44c0
SaneMyburg <sane.myburg@gmail.com>
2023-04-11 12:57:36 +0200

    Display rockets list in UI

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,35 @@
-const Rockets = () => (
-  <section>
-    Rockets
-  </section>
-);
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRockets } from '../redux/features/rockets/rocketsSlice';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const status = useSelector((state) => state.rockets.status);
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'success' && (
+      <>
+        {rockets.map((rocket) => (
+          <div key={rocket.id}>
+            <h2>{rocket.rocket_name}</h2>
+            <p>{rocket.description}</p>
+            {rocket.flickr_images.length > 0 && (
+            <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+            )}
+          </div>
+        ))}
+      </>
+      )}
+      {status === 'rejected' && <div>Failed to load rockets data</div>}
+    </div>
+  );
+};
+
 export default Rockets;
