148fbd9bde003409e06d69df7e91184b351a1c62
Nahom <zerihunnahom@gmail.com>
2023-04-11 16:34:59 +0300

    Modify App component to dispatch getMissions in it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getMissions } from './redux/features/missions/missionsSlice';
 import Rockets from './components/Rockets';
 import NavBar from './components/NavBar';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, []);
   return (
     <BrowserRouter>
       <div className="App">
