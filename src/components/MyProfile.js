d950ecf7b961238ddf174cabfe3334102ff3e66b
SaneMyburg <sane.myburg@gmail.com>
2023-04-11 23:56:22 +0200

    Add list of reserved rockets

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,6 +1,25 @@
-const MyProfile = () => (
-  <section>
-    profile
-  </section>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectReservedRockets } from '../redux/features/rockets/rocketsSlice';
+
+const MyProfile = () => {
+  const reservedRockets = useSelector(selectReservedRockets);
+  return (
+    <section>
+      <h2>My Rockets</h2>
+      {reservedRockets.length > 0 ? (
+        <div>
+          {reservedRockets.map((rocket) => (
+            <div key={rocket.id}>
+              <p>{rocket.name}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>No Rockets reserved</div>
+      )}
+    </section>
+  );
+};
+
 export default MyProfile;
