f3e8831f995efa54a027bc03273a67b6d42e8bc5
Nahom <zerihunnahom@gmail.com>
2023-04-12 11:24:20 +0300

    Add styling to Missions component

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -5,25 +5,29 @@ const Missions = () => {
   const { missions, isLoading } = useSelector((store) => store.missions);
   const dispatch = useDispatch();
   return (
-    <section>
+    <section className="missions-container">
       {isLoading ? (
         <div className="loading">Loading...</div>
       ) : (
         <div>
-          <table>
-            <tr>
-              <th>Mission</th>
-              <th>Description</th>
-              <th>Status</th>
-              <th>{' '}</th>
+          <table className="missions">
+            <tr className="mission-table-row">
+              <th className="mission-table-header">Mission</th>
+              <th className="mission-table-header">Description</th>
+              <th className="mission-table-header">Status</th>
+              <th className="mission-table-header">{' '}</th>
             </tr>
             {missions.map((mission) => (
-              <tr key={mission.mission_id}>
-                <td>{mission.mission_name}</td>
-                <td>{mission.description}</td>
-                <td>{mission.isMember ? 'Active Member' : 'Not A Member'}</td>
-                <td>
-                  <button type="button" onClick={() => dispatch(joinMission(mission.mission_id))}>{mission.isMember ? 'Leave Mission' : 'Join Mission'}</button>
+              <tr key={mission.mission_id} className="mission-table-row">
+                <td className="mission-table-data">{mission.mission_name}</td>
+                <td className="mission-table-data">{mission.description}</td>
+                <td className="mission-table-data">
+                  <span className={mission.reserved ? 'member' : 'non-member'}>
+                    {mission.reserved ? 'Active Member' : 'NOT A MEMBER'}
+                  </span>
+                </td>
+                <td className="mission-table-data">
+                  <button type="button" className={mission.reserved ? 'leave-button' : 'join-button'} onClick={() => dispatch(joinMission(mission.mission_id))}>{mission.reserved ? 'Leave Mission' : 'Join Mission'}</button>
                 </td>
               </tr>
             ))}
