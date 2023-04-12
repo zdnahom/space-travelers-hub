0956dbbbc37bc72bcf0203c852d3d5b445ce0a67
Nahom <zerihunnahom@gmail.com>
2023-04-13 02:41:23 +0300

    Edit class names

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -5,19 +5,19 @@ const MyProfile = () => {
   const joinedMissions = missions.filter((mission) => mission.reserved === true);
   return (
     <section className="profile">
-      <div className="profile-missions-section">
-      <h2 className="profile-mission-header">My Missions</h2>
-      {joinedMissions.length > 0 ? (
-        <ul className='reserved-missions-container'>
-          {joinedMissions.map((mission) => (
-            <li key={mission.mission_id} className="reserved-mission">
-              <span>{mission.mission_name}</span>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div>No Rockets reserved</div>
-      )}
+      <div>
+        <h2 className="profile-headline-text">My Missions</h2>
+        {joinedMissions.length > 0 ? (
+          <ul className="reserved-items-container">
+            {joinedMissions.map((mission) => (
+              <li key={mission.mission_id} className="reserved-item">
+                <span>{mission.mission_name}</span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>No Rockets reserved</div>
+        )}
       </div>
     </section>
   );
