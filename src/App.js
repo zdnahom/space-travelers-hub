0043e399bd90624983a8cd052a7cbb9b0b34ecf3
Nahom <zerihunnahom@gmail.com>
2023-04-12 19:56:47 +0300

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getMissions } from './redux/features/missions/missionsSlice';
 import Rockets from './components/Rockets';
 import NavBar from './components/NavBar';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getMissions());
-  }, [dispatch]);
   return (
     <BrowserRouter>
       <div className="App">
