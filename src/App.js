0feda684a44c2cebabc69092db4ea895f60cf9aa
Nahom <zerihunnahom@gmail.com>
2023-04-12 13:06:16 +0300

    Fix linter

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getMissions());
-  }, []);
+  }, [dispatch]);
   return (
     <BrowserRouter>
       <div className="App">
